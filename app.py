8df3a8faf58624c533d2a3fccfdae1d476f923b0
Rahul Krishna S <146613299+rahulkrishna10@users.noreply.github.com>
2024-04-18 11:25:16 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -23,19 +23,11 @@ def spec_to_db(y):
 
 # Function to preprocess audio file
 def preprocess_audio(audio_path):
-    # Read audio file
     y = read_file(audio_path)
-    
-    # Compute spectrogram
     spectrogram = tf.abs(tf.signal.stft(y, frame_length=512, frame_step=64))
-    
-    # Convert to dB scale
     spectrogram_db = spec_to_db(spectrogram)
     spectrogram_db = spectrogram_db / 80 + 1
-    
-    # Expand dimensions to match model input shape
     spectrogram_db = np.expand_dims(spectrogram_db, axis=0)
-    
     return spectrogram_db
 
 # Load the trained model
@@ -50,74 +42,40 @@ class_names = {
     4: "Northern Cardinal"
 }
 
-page = st.selectbox("Select a page:", ["Classification", "Evaluation"])
+def display_spectrogram(audio_data):
+    fig, ax = plt.subplots(figsize=(10, 4))
+    librosa.display.specshow(audio_data[0], sr=22050, x_axis='time', y_axis='log')
+    plt.colorbar(format='%+2.0f dB')
+    plt.title('Spectrogram')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Frequency (Hz)')
+    st.pyplot(fig)
 
-if page == "Classification":
+def predict_bird_species(uploaded_file):
+    audio_data = preprocess_audio(uploaded_file)
+    st.audio(uploaded_file, format='audio/wav', start_time=0)
+    display_spectrogram(audio_data)
+    prediction = model.predict(audio_data)
+    predicted_class_index = np.argmax(prediction)
+    predicted_species = class_names[predicted_class_index]
+    st.markdown(f"#### Predicted Bird Species: {predicted_species}")
 
-    st.title("Bird Species Classification")
-
-    bird_species = [
-        "Bewick's Wren",
-        "Northern Mockingbird",
-        "American Robin",
-        "Song Sparrow",
-        "Northern Cardinal"
-    ]
-    # Display the list of bird species with monospace font
-    st.markdown("*Bird species that can be classified: `" + "`, `".join(bird_species) + "`", unsafe_allow_html=True)
-
-
-    # File uploader
-    uploaded_file = st.file_uploader("Upload an audio file", type=["wav"])
-
-    predict_button = st.button("Predict")
-
-    # Perform inference when file is uploaded
-    if uploaded_file is not None and predict_button:
-        # Preprocess the uploaded audio file
-        audio_data = preprocess_audio(uploaded_file)
-        
-        # Display audio player
-        st.audio(uploaded_file, format='audio/wav', start_time=0)
-        
-        # Display spectrogram
-        fig, ax = plt.subplots(figsize=(10, 4))
-        librosa.display.specshow(audio_data[0], sr=22050, x_axis='time', y_axis='log')
-        plt.colorbar(format='%+2.0f dB')
-        plt.title('Spectrogram')
-        plt.xlabel('Time (s)')
-        plt.ylabel('Frequency (Hz)')
-        st.pyplot(fig)
-        
-        # Perform inference using the model
-        prediction = model.predict(audio_data)
-        
-        # Get the predicted class index
-        predicted_class_index = np.argmax(prediction)
-        
-        # Get the predicted bird species name
-        predicted_species = class_names[predicted_class_index]
-        
-        # Display the predicted bird species
-        st.markdown(f"#### Predicted Bird Species: {predicted_species}")
-
-elif page == 'Evaluation':
+def display_evaluation():
     st.title("Model Evaluation")
-
     with open('accuracy.json', 'r') as f:
         accuracy_data = json.load(f)
 
     st.subheader("Model Evaluation Metrics")
 
     metrics = {
-    "Metric": ["Accuracy", "F1 Score", "Precision", "Recall", "AUC"],
-    "Value": [
-        accuracy_data['accuracy'],
-        accuracy_data['F1'],
-        accuracy_data['Precision'],
-        accuracy_data['Recall'],
-        accuracy_data['AUC']
-    ]
+        "Metric": ["Accuracy", "F1 Score", "Precision", "Recall", "AUC"],
+        "Value": [
+            accuracy_data['accuracy'],
+            accuracy_data['F1'],
+            accuracy_data['Precision'],
+            accuracy_data['Recall'],
+            accuracy_data['AUC']
+        ]
     }
 
     st.table(metrics)
@@ -128,15 +86,23 @@ elif page == 'Evaluation':
     labels = ['Bewick\'s Wren', 'Northern Mockingbird', 'American Robin', 'Song Sparrow', 'Northern Cardinal']
 
     plt.figure(figsize=(15, 8))
-    sns.heatmap(conf_matrix_df, 
-            annot=True, 
-            fmt='g', 
-            cmap='Blues', 
-            xticklabels=labels, 
-            yticklabels=labels)
-
+    sns.heatmap(conf_matrix_df, annot=True, fmt='g', cmap='Blues', xticklabels=labels, yticklabels=labels)
     plt.xlabel('Prediction')
     plt.ylabel('Label')
 
-    fig = plt.gcf()
+    # Pass the figure explicitly to st.pyplot()
+    fig, _ = plt.gcf(), plt.gca()
     st.pyplot(fig)
+
+page = st.selectbox("Select a page:", ["Classification", "Evaluation"])
+
+if page == "Classification":
+    st.title("Bird Species Classification")
+    bird_species = list(class_names.values())
+    st.markdown("*Bird species that can be classified: `" + "`, `".join(bird_species) + "`", unsafe_allow_html=True)
+    uploaded_file = st.file_uploader("Upload an audio file", type=["wav"])
+    predict_button = st.button("Predict")
+    if uploaded_file is not None and predict_button:
+        predict_bird_species(uploaded_file)
+elif page == 'Evaluation':
+    display_evaluation()
